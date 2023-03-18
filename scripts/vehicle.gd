31df1572329ceeb06ae6a1ab1d034ec2d910ce36
abam-iksde <assdlik@gmail.com>
2023-03-18 03:14:56 +0100

    Introduce basic collision response

--- scripts/vehicle.gd
+++ scripts/vehicle.gd
@@ -3,11 +3,15 @@ extends CharacterBody3D
 
 const STEERING_THRESHOLD = 0.2
 const DRIFT_EXIT_THRESHOLD = deg_to_rad(15)
+const GRIPPING_TIME_AFTER_HIT = 0.3
+const GRIPPING_TIME_AFTER_HANDBRAKE = 0.1
 
 @export var max_speed = 20
 @export var acceleration = 32
 @export var grip = 1
 @export var damping = 16
+
+var gripping_time = 0
 
 var scalar_speed = 0
 
@@ -17,6 +21,7 @@ enum MovementMode {
 }
 
 var movement_mode = MovementMode.NORMAL
+var slide_steering_multiplier = 1
 
 func get_input():
 	return {
@@ -27,10 +32,12 @@ func get_input():
 
 func normal(delta, input):
 	if input['handbrake']:
+		slide_steering_multiplier = sign(scalar_speed)
 		movement_mode = MovementMode.SLIDING
+		gripping_time = GRIPPING_TIME_AFTER_HANDBRAKE
 	
 	if velocity.length_squared() > STEERING_THRESHOLD:
-		$mesh_instance.rotation.y += input['steering'] * grip * 3 * delta
+		$mesh_instance.rotation.y += input['steering'] * sign(scalar_speed) * grip * 3 * delta
 	
 	scalar_speed += acceleration * input['acceleration'] * delta
 	scalar_speed -= sign(scalar_speed) * damping * delta
@@ -39,19 +46,34 @@ func normal(delta, input):
 	velocity = Vector3(0, 0, scalar_speed).rotated(Vector3(0, 1, 0), $mesh_instance.rotation.y)
 
 func slide(delta, input):
+	gripping_time -= delta
 	if not input['handbrake']:
 		if velocity.angle_to(Vector3(0, 0, 1).rotated(Vector3(0, 1, 0), $mesh_instance.rotation.y)) < DRIFT_EXIT_THRESHOLD:
-			scalar_speed = velocity.length()
-			movement_mode = MovementMode.NORMAL
+			if gripping_time <= 0:
+				scalar_speed = velocity.length()
+				movement_mode = MovementMode.NORMAL
 		elif velocity.angle_to(Vector3(0, 0, -1).rotated(Vector3(0, 1, 0), $mesh_instance.rotation.y)) < DRIFT_EXIT_THRESHOLD:
-			scalar_speed = -velocity.length()
-			movement_mode = MovementMode.NORMAL
+			if gripping_time <= 0:
+				scalar_speed = -velocity.length()
+				movement_mode = MovementMode.NORMAL
+		else:
+			gripping_time = max(GRIPPING_TIME_AFTER_HANDBRAKE, gripping_time)
+	else:
+		gripping_time = max(GRIPPING_TIME_AFTER_HANDBRAKE, gripping_time)
 	
 	if velocity.length_squared() > STEERING_THRESHOLD:
-		$mesh_instance.rotation.y += input['steering'] * grip * 5 * delta
+		$mesh_instance.rotation.y += input['steering'] * grip * 5 * delta * slide_steering_multiplier
 	
 	velocity += Vector3(0, 0, acceleration * input['acceleration']).rotated(Vector3(0, 1, 0), $mesh_instance.rotation.y) * delta
 	velocity -= velocity.normalized() * damping * delta
+
+func check_collisions():
+	if get_slide_collision_count() == 0:
+		return
+	var collision = get_last_slide_collision()
+	velocity = velocity.bounce(collision.get_normal())
+	movement_mode = MovementMode.SLIDING
+	gripping_time = GRIPPING_TIME_AFTER_HIT
 
 func _physics_process(delta):
 	var input = get_input()
@@ -61,6 +83,8 @@ func _physics_process(delta):
 			normal(delta, input)
 		MovementMode.SLIDING:
 			slide(delta, input)
+	
+	check_collisions()
 	
 	if velocity.length_squared() < STEERING_THRESHOLD:
 		velocity = Vector3()
