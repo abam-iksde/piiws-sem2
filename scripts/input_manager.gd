0ec6a2fe2fd4859b1f10e5c012ec0b1a47d6007d
abam-iksde <assdlik@gmail.com>
2023-03-20 21:40:39 +0100

    Replace tabs with spaces

--- scripts/input_manager.gd
+++ scripts/input_manager.gd
@@ -2,8 +2,8 @@ class_name InputManager
 extends Object
 
 func get_input():
-	return {
-		'acceleration': 0,
-		'steering': 0,
-		'handbrake': false,
-	}
+  return {
+    'acceleration': 0,
+    'steering': 0,
+    'handbrake': false,
+  }
--- scripts/input_manager_human.gd
+++ scripts/input_manager_human.gd
@@ -4,11 +4,11 @@ extends InputManager
 var prefix
 
 func _init(_prefix):
-	prefix = _prefix
+  prefix = _prefix
 
 func get_input():
-	return {
-		'acceleration': Input.get_axis(prefix + '_backward', prefix + '_forward'),
-		'steering': Input.get_axis(prefix + '_right', prefix + '_left'),
-		'handbrake': Input.is_action_pressed(prefix + '_handbrake'),
-	}
+  return {
+    'acceleration': Input.get_axis(prefix + '_backward', prefix + '_forward'),
+    'steering': Input.get_axis(prefix + '_right', prefix + '_left'),
+    'handbrake': Input.is_action_pressed(prefix + '_handbrake'),
+  }
