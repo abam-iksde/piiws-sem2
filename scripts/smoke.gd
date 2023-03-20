8d3acfcec1eabc1c7454f0c114548aa637cd8c99
Adam Sadlik <72628208+abam-iksde@users.noreply.github.com>
2023-03-20 21:41:48 +0100

    🪐 Change indentation to 2 spaces (#6)

--- scripts/smoke.gd
+++ scripts/smoke.gd
@@ -3,11 +3,11 @@ extends Sprite3D
 var life_time = 0.5
 
 func _ready():
-	rotation_degrees.y = randi() % 4 * 90
+  rotation_degrees.y = randi() % 4 * 90
 
 func _physics_process(delta):
-	life_time -= delta
-	if life_time <= 0:
-		queue_free()
-	var time_spent = (0.5-life_time)/0.5
-	frame = min(floor(time_spent * 4), 3)
+  life_time -= delta
+  if life_time <= 0:
+    queue_free()
+  var time_spent = (0.5-life_time)/0.5
+  frame = min(floor(time_spent * 4), 3)
--- scripts/vehicle.gd
+++ scripts/vehicle.gd
@@ -19,8 +19,8 @@ var scalar_speed = 0
 var velocity_last_frame = null
 
 enum MovementMode {
-	NORMAL,
-	SLIDING,
+  NORMAL,
+  SLIDING,
 }
 
 var movement_mode = MovementMode.NORMAL
@@ -29,83 +29,83 @@ var slide_steering_multiplier = 1
 var input_manager = InputManagerHuman.new('p1')
 
 func normal(delta, input):
-	if input['handbrake']:
-		slide_steering_multiplier = sign(scalar_speed)
-		movement_mode = MovementMode.SLIDING
-		gripping_time = GRIPPING_TIME_AFTER_HANDBRAKE
-	
-	if velocity.length_squared() > STEERING_THRESHOLD:
-		$sprite.rotation.y += input['steering'] * sign(scalar_speed) * grip * 3 * delta
-	
-	scalar_speed += acceleration * input['acceleration'] * delta
-	scalar_speed -= sign(scalar_speed) * damping * delta
-	if abs(scalar_speed) > max_speed:
-		scalar_speed = max_speed * sign(scalar_speed)
-	velocity = Vector3(0, 0, scalar_speed).rotated(Vector3(0, 1, 0), $sprite.rotation.y)
+  if input['handbrake']:
+    slide_steering_multiplier = sign(scalar_speed)
+    movement_mode = MovementMode.SLIDING
+    gripping_time = GRIPPING_TIME_AFTER_HANDBRAKE
+  
+  if velocity.length_squared() > STEERING_THRESHOLD:
+    $sprite.rotation.y += input['steering'] * sign(scalar_speed) * grip * 3 * delta
+  
+  scalar_speed += acceleration * input['acceleration'] * delta
+  scalar_speed -= sign(scalar_speed) * damping * delta
+  if abs(scalar_speed) > max_speed:
+    scalar_speed = max_speed * sign(scalar_speed)
+  velocity = Vector3(0, 0, scalar_speed).rotated(Vector3(0, 1, 0), $sprite.rotation.y)
 
 func slide(delta, input):
-	gripping_time -= delta
-	if not input['handbrake']:
-		if velocity.angle_to(Vector3(0, 0, 1).rotated(Vector3(0, 1, 0), $sprite.rotation.y)) < DRIFT_EXIT_THRESHOLD:
-			if gripping_time <= 0:
-				scalar_speed = velocity.length()
-				movement_mode = MovementMode.NORMAL
-		elif velocity.angle_to(Vector3(0, 0, -1).rotated(Vector3(0, 1, 0), $sprite.rotation.y)) < DRIFT_EXIT_THRESHOLD:
-			if gripping_time <= 0:
-				scalar_speed = -velocity.length()
-				movement_mode = MovementMode.NORMAL
-		else:
-			gripping_time = max(GRIPPING_TIME_AFTER_HANDBRAKE, gripping_time)
-	else:
-		gripping_time = max(GRIPPING_TIME_AFTER_HANDBRAKE, gripping_time)
-	
-	if velocity.length_squared() > STEERING_THRESHOLD:
-		$sprite.rotation.y += input['steering'] * grip * 5 * delta * slide_steering_multiplier
-	
-	velocity += Vector3(0, 0, acceleration * 0.8 * input['acceleration']).rotated(Vector3(0, 1, 0), $sprite.rotation.y) * delta
-	velocity -= velocity.normalized() * damping * 1.5 * delta
+  gripping_time -= delta
+  if not input['handbrake']:
+    if velocity.angle_to(Vector3(0, 0, 1).rotated(Vector3(0, 1, 0), $sprite.rotation.y)) < DRIFT_EXIT_THRESHOLD:
+      if gripping_time <= 0:
+        scalar_speed = velocity.length()
+        movement_mode = MovementMode.NORMAL
+    elif velocity.angle_to(Vector3(0, 0, -1).rotated(Vector3(0, 1, 0), $sprite.rotation.y)) < DRIFT_EXIT_THRESHOLD:
+      if gripping_time <= 0:
+        scalar_speed = -velocity.length()
+        movement_mode = MovementMode.NORMAL
+    else:
+      gripping_time = max(GRIPPING_TIME_AFTER_HANDBRAKE, gripping_time)
+  else:
+    gripping_time = max(GRIPPING_TIME_AFTER_HANDBRAKE, gripping_time)
+  
+  if velocity.length_squared() > STEERING_THRESHOLD:
+    $sprite.rotation.y += input['steering'] * grip * 5 * delta * slide_steering_multiplier
+  
+  velocity += Vector3(0, 0, acceleration * 0.8 * input['acceleration']).rotated(Vector3(0, 1, 0), $sprite.rotation.y) * delta
+  velocity -= velocity.normalized() * damping * 1.5 * delta
 
 func check_collisions():
-	if get_slide_collision_count() == 0 or velocity.length_squared() < 0.5:
-		return
-	var collision = get_last_slide_collision()
-	velocity = velocity_last_frame.bounce(collision.get_normal()) * FORCE_HIT_MULTIPLIER
-	movement_mode = MovementMode.SLIDING
-	gripping_time = GRIPPING_TIME_AFTER_HIT
-	slide_steering_multiplier = 1
+  if get_slide_collision_count() == 0 or velocity.length_squared() < 0.5:
+    return
+  var collision = get_last_slide_collision()
+  velocity = velocity_last_frame.bounce(collision.get_normal()) * FORCE_HIT_MULTIPLIER
+  movement_mode = MovementMode.SLIDING
+  gripping_time = GRIPPING_TIME_AFTER_HIT
+  slide_steering_multiplier = 1
 
 func process_smoke():
-	var timer = get_tree().create_timer(0.02)
-	timer.connect('timeout', process_smoke)
-	if movement_mode != MovementMode.SLIDING:
-		return
-	for child in %tyres.get_children():
-		var instance = preload('res://scenes/prefabs/smoke.tscn').instantiate()
-		get_node('..').add_child(instance)
-		instance.global_position = child.global_position
+  var timer = get_tree().create_timer(0.02)
+  timer.connect('timeout', process_smoke)
+  if movement_mode != MovementMode.SLIDING:
+    return
+  for child in %tyres.get_children():
+    var instance = preload('res://scenes/prefabs/smoke.tscn').instantiate()
+    get_node('..').add_child(instance)
+    instance.global_position = child.global_position
 
 func _ready():
-	process_smoke.call_deferred()
+  process_smoke.call_deferred()
 
 func _physics_process(delta):
-	var input = input_manager.get_input()
-	
-	match movement_mode:
-		MovementMode.NORMAL:
-			normal(delta, input)
-		MovementMode.SLIDING:
-			slide(delta, input)
-	
-	check_collisions()
-	
-	if velocity.length_squared() < STEERING_THRESHOLD:
-		if movement_mode != MovementMode.NORMAL:
-			movement_mode = MovementMode.NORMAL
-			scalar_speed = 0
-		if input['acceleration'] == 0:
-			scalar_speed = 0
-	elif velocity.length_squared() > max_speed * max_speed:
-		velocity = velocity.normalized() * max_speed
-	
-	velocity_last_frame = velocity
-	move_and_slide()
+  var input = input_manager.get_input()
+  
+  match movement_mode:
+    MovementMode.NORMAL:
+      normal(delta, input)
+    MovementMode.SLIDING:
+      slide(delta, input)
+  
+  check_collisions()
+  
+  if velocity.length_squared() < STEERING_THRESHOLD:
+    if movement_mode != MovementMode.NORMAL:
+      movement_mode = MovementMode.NORMAL
+      scalar_speed = 0
+    if input['acceleration'] == 0:
+      scalar_speed = 0
+  elif velocity.length_squared() > max_speed * max_speed:
+    velocity = velocity.normalized() * max_speed
+  
+  velocity_last_frame = velocity
+  move_and_slide()
