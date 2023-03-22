a60257b876306eb3668ba2ac2790e3841773b9d2
Adam Sadlik <72628208+abam-iksde@users.noreply.github.com>
2023-03-22 22:36:57 +0100

    ↗️ Add diagonal directions to `path_map` (#10)

--- scripts/angles.gd
+++ scripts/angles.gd
@@ -4,3 +4,7 @@ const right = deg_to_rad(90)
 const left = deg_to_rad(-90)
 const up = deg_to_rad(180)
 const down = 0
+const diagonal_down_right = deg_to_rad(45)
+const diagonal_up_left = deg_to_rad(-135)
+const diagonal_up_right = deg_to_rad(135)
+const diagonal_down_left = deg_to_rad(-45)
