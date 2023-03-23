afd5c1f87fb5a6674f082bf2b54c78b9b74391bc
abam-iksde <assdlik@gmail.com>
2023-03-23 20:28:53 +0100

    Refactor `angles.gd` & extract vehicle constants

--- scripts/angles.gd
+++ scripts/angles.gd
@@ -8,3 +8,24 @@ const diagonal_down_right = deg_to_rad(45)
 const diagonal_up_left = deg_to_rad(-135)
 const diagonal_up_right = deg_to_rad(135)
 const diagonal_down_left = deg_to_rad(-45)
+
+func get_angle_from_index(_index):
+  var index = wrapi(int(_index), 0, 8)
+  match index:
+    0:
+      return right
+    1:
+      return left
+    2:
+      return up
+    3:
+      return down
+    4:
+      return diagonal_down_right
+    5:
+      return diagonal_up_left
+    6:
+      return diagonal_up_right
+    7:
+      return diagonal_down_left
+  return null
