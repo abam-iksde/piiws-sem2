b5e49f9f3b12f69eb127529c874796b21f59f1d1
abam-iksde <assdlik@gmail.com>
2023-03-26 03:10:40 +0200

    Add lap counting

---
+++ scripts/checkpoint.gd
@@ -0,0 +1,11 @@
+extends Area3D
+
+func on_body_enter(body):
+  if body is Vehicle:
+    if body.next_checkpoint == self:
+      body.collect_checkpoint(false)
+    elif body.previous_checkpoint == self:
+      body.collect_checkpoint(true)
+
+func _ready():
+  connect('body_entered', on_body_enter)
---
+++ scripts/checkpoints_setter.gd
@@ -0,0 +1,4 @@
+extends Node3D
+
+func _ready():
+  Race.init(get_children(), 3)
