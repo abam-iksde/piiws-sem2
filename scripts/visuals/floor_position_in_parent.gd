c8692c2262f97421005e6d9dbe517b9b660490c8
abam-iksde <assdlik@gmail.com>
2023-04-02 14:56:52 +0200

    Add basic split-screen

--- scripts/visuals/floor_position_in_parent.gd
+++ scripts/visuals/floor_position_in_parent.gd
@@ -1,9 +1,15 @@
+class_name FloorInParent
 extends Node3D
+
+@export var parent: Node3D
 
 func _process(delta):
   if not GameSettings.pixelated_look:
     return
-  var parent_position = get_node('..').global_position
+  var _parent = parent
+  if _parent == null:
+    _parent = get_parent()
+  var parent_position = _parent.global_position
   global_position = Vector3(
     floor(parent_position.x*16.0)/16.0,
     global_position.y,
---
+++ scripts/visuals/world_viewport_container_splitscreen.gd
@@ -0,0 +1,19 @@
+extends Sprite2D
+
+@export var camera_screen_offset: Vector2 = Vector2(-0.25, 0.0)
+@export var camera_parent: Node3D
+
+func on_resize():
+  var viewport_size = get_viewport().size
+  global_position = viewport_size / 2.0
+  $viewport.size = Vector2(320.0 / viewport_size.y * viewport_size.x, 320.0)
+  scale = Vector2(viewport_size.x, viewport_size.y) / Vector2(texture.get_size().x, texture.get_size().y)
+
+func _ready():
+  on_resize()
+  get_viewport().connect('size_changed', on_resize)
+  $viewport/camera_holder.screen_offset = camera_screen_offset
+  $viewport/camera_holder.parent = camera_parent
+  
+  material = material.duplicate()
+  material.set_shader_parameter('viewport_texture', $viewport.get_texture())
