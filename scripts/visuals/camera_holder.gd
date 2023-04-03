a2201864dfb6eb8001fe2732cd428bbea2b1cd12
Adam Sadlik <72628208+abam-iksde@users.noreply.github.com>
2023-04-03 22:56:38 +0200

    🔪 Fix non-pixelated look (#33)

--- scripts/visuals/camera_holder.gd
+++ scripts/visuals/camera_holder.gd
@@ -1,8 +1,11 @@
 extends FloorInParent
 
+const SCREEN_WIDTH_IN_TILES = 426.0 / 16.0
+const SCREEN_HEIGHT_IN_TILES = 320.0 / 16.0
+
 @export var screen_offset: Vector2 = Vector2(0.0, 0.0)
 
 func _process(delta):
   super._process(delta)
-  $camera.position.x = screen_offset.x * get_viewport().size.x / 16.0
-  $camera.position.z = screen_offset.y * get_viewport().size.y / 16.0
+  $camera.position.x = screen_offset.x * SCREEN_WIDTH_IN_TILES
+  $camera.position.z = screen_offset.y * SCREEN_HEIGHT_IN_TILES
