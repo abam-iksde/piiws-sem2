dd1f5af22df61cae34b3380159b86b514045aab8
Adam Sadlik <72628208+abam-iksde@users.noreply.github.com>
2023-04-12 20:42:46 +0200

    🎯 Keep player in the center of splitscreen view (#34)

--- scripts/visuals/camera_holder.gd
+++ scripts/visuals/camera_holder.gd
@@ -1,11 +1,12 @@
 extends FloorInParent
 
-const SCREEN_WIDTH_IN_TILES = 426.0 / 16.0
 const SCREEN_HEIGHT_IN_TILES = 320.0 / 16.0
 
 @export var screen_offset: Vector2 = Vector2(0.0, 0.0)
 
 func _process(delta):
   super._process(delta)
-  $camera.position.x = screen_offset.x * SCREEN_WIDTH_IN_TILES
+  var viewport_size = get_viewport().size
+  var screen_width = 320.0/viewport_size.y * viewport_size.x
+  $camera.position.x = screen_offset.x * screen_width / 16.0
   $camera.position.z = screen_offset.y * SCREEN_HEIGHT_IN_TILES
