2653b7a8bf3117130f61d6db8b2e11427dcc9f7b
abam-iksde <assdlik@gmail.com>
2023-04-24 00:28:19 +0200

    Extract `build_map` components

--- scripts/game/world.gd
+++ scripts/game/world.gd
@@ -6,8 +6,15 @@ const DirectionBoxScene = preload('res://scenes/prefabs/game/direction_box.tscn'
 func _ready():
   var loader = MapLoader.new()
   var map_data = loader.load_map('res://maps/test_map/map.json', 'res://maps/test_map/set.png')
-  build_visuals(map_data)
+  build_map(map_data)
   Race.init.call_deferred($checkpoints.get_children())
+
+func build_map(map_data):
+  build_visuals(map_data)
+  build_start_spots(map_data)
+  build_path_map(map_data)
+  build_walls(map_data)
+  build_checkpoints(map_data)
 
 func build_visuals(map_data):
   for layer in map_data[0].visual:
@@ -22,18 +29,26 @@ func build_visuals(map_data):
       tile_node.set_surface_override_material(0, material)
       layer_node.add_child(tile_node)
     $visuals.add_child(layer_node)
+
+func build_start_spots(map_data):
   for start_spot in map_data[0].start_spots:
     var spot_node = Node3D.new()
     spot_node.position = Vector3(start_spot.x, 0, start_spot.y)
     $start_spots.add_child(spot_node)
+
+func build_path_map(map_data):
   for direction in map_data[0].directions:
     var direction_box = DirectionBoxScene.instantiate()
     direction_box.position = Vector3(direction.position.x, 0, direction.position.y)
     direction_box.direction = direction.direction
     $path_map.add_child(direction_box)
+
+func build_walls(map_data):
   for wall in map_data[0].walls:
     var wall_collision = build_wall_collision_shape(wall)
     $walls.add_child(wall_collision)
+
+func build_checkpoints(map_data):
   for checkpoint in map_data[0].checkpoints:
     var checkpoint_node = Checkpoint.new()
     var point_collision = build_wall_collision_shape(checkpoint.shape)
