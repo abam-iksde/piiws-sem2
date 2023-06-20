3a15987f9a51c59e2a63b3da1fc9905ff78414cf
abam-iksde <assdlik@gmail.com>
2023-06-20 23:28:44 +0200

    Extract magic numbers in `explosion.gd`

--- scripts/game/explosion.gd
+++ scripts/game/explosion.gd
@@ -1,5 +1,7 @@
 extends Area3D
 
+const EXPLOSION_DURATION = 0.5
+
 func explode():
   for body in get_overlapping_bodies():
     if body is Vehicle:
@@ -11,9 +13,9 @@ var time = 0
 
 func _physics_process(delta):
   time += delta
-  if time > 0.5:
+  if time > EXPLOSION_DURATION:
     queue_free()
-  $sprite.frame = int(time * 8) % 4
+  $sprite.frame = int(time * (1.0/EXPLOSION_DURATION) * $sprite.hframes) % $sprite.hframes
   if phase != 1:
     if phase == 0:
       phase = 1
